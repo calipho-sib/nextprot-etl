cda51bb5f18503f12d2cb3b49ef38a34f798b5e3
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2022-07-07 19:07:32 +0200

    Load commons as beans

--- src/main/java/org/nextprot/etl/ETLRunner.java
+++ src/main/java/org/nextprot/etl/ETLRunner.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Base ETL Runner, which spawns up a kafka consumer & producer accordingly, which runs E.T.L portions of the pipeline
  */
 @ComponentScan(basePackages = "org.nextprot.etl")
+@ImportResource("classpath:commons-context.xml")
 public class ETLRunner {
 
     public static void main(String args[]) {
--- src/main/java/org/nextprot/etl/kafka/Consumer.java
+++ src/main/java/org/nextprot/etl/kafka/Consumer.java
@@ -1,13 +1,20 @@
 package org.nextprot.etl.kafka;
 
+import org.nextprot.api.etl.service.StatementETLService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-//@ComponentScan(basePackages = "org.nextprot.api.etl.service")
+@Configuration
+@ComponentScan(basePackages = {"org.nextprot.api.*", "org.nextprot.commons.*"})
 @Component
 public class Consumer {
+
+    @Autowired
+    StatementETLService etlService;
 
     public void doSomething() {
        /*try {
